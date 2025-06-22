f0fe7b27e71d3e99d24edd701736d04305e9ef4f
cyy <chenyingying2003@outlook.com>
2025-06-22 20:52:26 +0800

    feat: 初步配置路由

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+// 关于路由的配置
+import Vue from "vue" // 为什么引入vue
+import Router from 'vue-router'
+import Layout from '@/Layout'
+import { component } from "vue/types/umd"
+
+Vue.use(Router)
+
+const constantRoutes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/home',
+        name: '首页',
+        meta: { title: '首页' }, // 一些前端自定义路由的数据/属性
+        // 使用路由懒加载，要知道懒加载的优势
+        // 既然要懒加载，之后就要开始创建页面
+        component: () => import('@/pages/home')
+      }
+    ]
+  }
+]
+
+export default new Router({ routes: constantRoutes })
